8b59f6e3761aba0b70e2dff42ea349b4a16307f4
daruna <f0rever@mail.ru>
2015-04-22 01:30:48 +0400

    correct CheckSiteAvailability task

--- app/services/check_site_availability.rb
+++ app/services/check_site_availability.rb
@@ -2,7 +2,7 @@ class CheckSiteAvailability
   def perform
     Site.find_each do |site|
       available = site.available?
-      site.update_attributes(available: available)
+      site.update_attributes!(available: available, updated_at: Time.zone.now)
     end
   end
 end
